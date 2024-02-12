b2a1ca2a8cbe590ecaffd2d11cc3751ea42154d0
AlexTec974 <alex@tec.tech>
2024-02-12 13:14:10 +0800

    fix: new eslint config need use require rather than import

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
-// eslint.config.js
-import antfu from '@antfu/eslint-config'
+const antfu = require('@antfu/eslint-config').default
 
-export default antfu({
-    vue: {}
+module.exports = antfu({
+  vue: {},
 })
