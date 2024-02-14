db2e3fd83fb102df97b6da46d8754ccdeb6f58ab
AlexTec974 <alex@tec.tech>
2024-02-14 17:04:59 +0800

    ci: add aditionnal lintin for vuejs

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,21 @@
 const antfu = require('@antfu/eslint-config').default
 
 module.exports = antfu({
-  vue: {},
+  vue: {
+    overrides: {
+      'vue/block-order': ['error', {
+        order: ['template', 'script', 'style'],
+      }],
+      'vue/html-self-closing': ['error', {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      }],
+    },
+
+  },
 })
