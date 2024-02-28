7e7bcbf89100a212d889a50da1454b96de4bb547
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2024-02-28 05:32:12 +0300

    (fix): removed speeddial (#9)

--- apps/web-nuxt/src/components/Sidebar.vue
+++ apps/web-nuxt/src/components/Sidebar.vue
@@ -10,68 +10,47 @@
       </button>
     </div>
 
-    <div 
-      class=" flex flex-col justify-between  h-full"
+    <div class=" flex flex-col justify-between  h-full"
       :class="{ 'w-20 justify-center overflow-x-hidden': isCollapsed, 'w-60': !isCollapsed, 'py-6': true }">
-      
+
       <ul class="mt-2">
         <!-- Logo -->
-        <div class="mb-7 ml-3 " :class="{'flex justify-center items-center mt-3': isCollapsed}">
+        <div class="mb-7 ml-3 " :class="{ 'flex justify-center items-center mt-2': isCollapsed }">
           <NuxtLink to="/">
 
-            <img 
-              v-if="!isCollapsed" 
-              src="../assets/Logo.png" 
-              class="w-40 h-auto" />
-            <img v-else 
-              src="../assets/LogoSquare.png" 
-              class=" w-14 h-10" />
-          
+            <img v-if="!isCollapsed" src="../assets/Logo.png" class="w-40 h-auto" />
+            <img v-else src="../assets/LogoSquare.png" class=" w-14 h-10" />
+
           </NuxtLink>
         </div>
         <!-- menu Items -->
-        <li 
-          v-for="item in menuItems" 
-          :key="item.title" 
-          class=" w-full cursor-pointer px-3 ml-2 "
+        <li v-for="item in menuItems" :key="item.title" class=" w-full cursor-pointer px-3 ml-2 "
           :class="{ '': isCollapsed, 'mb-2': !isCollapsed, 'border-l-2 text-primaryPurple border-primaryPurple': baseRoute === item.route }">
 
-          <div 
-            :key="item.title" class="hover:bg-primaryPurple hover:text-white rounded-md flex text-center items-center"
+          <div :key="item.title" class="hover:bg-primaryPurple hover:text-white rounded-md flex text-center items-center"
             :class="{ 'flex-col text-center items-center py-2 mr-2 text-sm': isCollapsed, 'my-2 py-3 px-3': !isCollapsed, }"
-            @click="navigate(item.route)" 
-            @mouseenter="setIsHovered(item, true)" 
-            @mouseleave="setIsHovered(item, false)">
+            @click="navigate(item.route)" @mouseenter="setIsHovered(item, true)" @mouseleave="setIsHovered(item, false)">
 
-            <i 
-              v-if="isCollapsed" 
-              style="font-size: 1.5rem" 
-              class="text-gray-500 text-center mb-2 py-3" 
-              :class="[
+            <i v-if="isCollapsed" style="font-size: 1.5rem" class="text-gray-500 text-center mb-2 py-3" :class="[
               item.icon, 'mr-2',
-                {
-                  'text-white': item.isHovered,
-                  'text-primaryPurple': baseRoute === item.route,
-                  'text-gray-500': !item.isHovered
-                }
-              ]">
+              {
+                'text-white': item.isHovered,
+                'text-primaryPurple': baseRoute === item.route,
+                'text-gray-500': !item.isHovered
+              }
+            ]">
             </i>
 
-            <i 
-              v-if="!isCollapsed" 
-              style="font-size: 1.4rem"
+            <i v-if="!isCollapsed" style="font-size: 1.4rem"
               :class="[item.icon, 'mr-2', { 'text-white': item.isHovered, 'text-primaryPurple': baseRoute === item.route, 'text-gray-500': !item.isHovered }]">
             </i>
 
-            <span 
-              v-if="!isCollapsed"
-              class=" text-lg font-medium text-gray-500 ml-2 " 
-              :class="{
-                'text-white': item.isHovered,
-               
-                'text-primaryPurple text-lg': baseRoute === item.route,
-                'text-gray-500': !item.isHovered 
-                }">
+            <span v-if="!isCollapsed" class=" text-lg font-medium text-gray-500 ml-2 " :class="{
+              'text-white': item.isHovered,
+
+              'text-primaryPurple text-lg': baseRoute === item.route,
+              'text-gray-500': !item.isHovered
+            }">
               {{ item.title }}
             </span>
           </div>
@@ -80,98 +59,70 @@
 
       <ul>
         <!-- settings -->
-        <li class="mt-7  px-4 cursor-pointer ml-2 border-t w-full  py-4 " >
-          
-          <div 
-            class="hover:bg-primaryPurple hover:text-white  rounded-md mt-2 flex  text-center items-center"
-            :class="{ '   mr-2 text-sm': isCollapsed, ' py-3 px-3': !isCollapsed, 'bg-primaryPurple': openSubMenu }"
-            @mouseenter="settingHovered = true"
-            @mouseleave="settingHovered = false"
-            @click="toggle">
+        <li class="mt-7  cursor-pointer ml-2 mr-2 w-full  py-4">
 
-         
+          <hr class="text-gray-300" />
+          <div class="px-3" :class="{ 'border-l-2 text-primaryPurple border-primaryPurple': openSubMenu }">
 
-              <i v-if="isCollapsed" 
-                class="pi pi-cog cursor-pointer text-gray-500  text-center mb-2 py-3" 
-                style="font-size: 1.5rem" 
-                :class="{
-                    'text-white': settingHovered || openSubMenu,
-                    }"></i>
-  
-                <i 
-                  v-if="!isCollapsed" 
-                  style="font-size: 1.4rem"
-                  class="pi pi-cog text-gray-500"
-                  :class="{
-                    'text-white': settingHovered || openSubMenu,
-                    }"
-                  >
-                </i>
-              <span 
-                  v-if="!isCollapsed"
-                  class=" text-lg font-medium text-gray-500 ml-2 " 
-                  :class="{
-                    'text-white': settingHovered || openSubMenu
-                    }"
-                  >
-                  Settings
-                </span>
+            <div class="hover:bg-primaryPurple hover:text-white  rounded-md mt-2 flex  text-center items-center"
+              :class="{ ' px-2  mr-2 text-sm': isCollapsed, ' py-3 px-3': !isCollapsed, 'text-primaryPurple': openSubMenu }"
+              @mouseenter="settingHovered = true" @mouseleave="settingHovered = false" @click="toggle">
+
+              <i v-if="isCollapsed" class="pi pi-cog cursor-pointer text-gray-500  text-center mb-2 py-3"
+                style="font-size: 1.5rem" :class="{
+                  'text-white': settingHovered,
+                  'text-primaryPurple': openSubMenu,
+                }"></i>
+
+              <i v-if="!isCollapsed" style="font-size: 1.4rem" class="pi pi-cog text-gray-500 mr-2" :class="{
+                'text-white': settingHovered,
+                'text-primaryPurple': openSubMenu,
+              }">
+              </i>
+              <span v-if="!isCollapsed" class=" text-lg font-medium text-gray-500 ml-2 " :class="{
+                'text-white': settingHovered,
+                'text-primaryPurple': openSubMenu,
+              }">
+                Settings
+              </span>
             </div>
-          
-       
-        </li>
-
-        <!-- speeddial -->
-        <li>
-          <div class="flex mb-2" :style="{ position: 'relative', height: '350px' }">
-            <Toast />
-            <SpeedDial :model="items" direction="up" :transitionDelay="80" showIcon="pi pi-bars" hideIcon="pi pi-times" showData="data" >
-                <template #item="{ item }">
-                  <!-- Customize the rendering of each SpeedDial item -->
-                  <div class=" mx-4 rounded-md flex  py-2 px-7  hover:bg-primaryPurple hover:text-white w-48 space-x-4"  @click="" >
-                    <i :class="item.icon" class="text-left"></i>
-                    <span>{{ item.text }}</span>
-                  </div>
-                </template>
-            </SpeedDial>
           </div>
+
         </li>
 
         <!-- avatar -->
-        <div 
-          class="flex  mt-auto px-4 "
-          :class="{'space-y-2 flex-col justify-center items-center': isCollapsed, 'space-x-2 ml-4': !isCollapsed }">
-          
+        <div class="flex  mt-auto px-4 "
+          :class="{ 'space-y-2 flex-col justify-center items-center': isCollapsed, 'space-x-2 ml-2': !isCollapsed }">
+
           <span class="relative inline-block">
-            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+            <img class="h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt="" />
             <span class="absolute right-0 top-0 block h-2.5 w-2.5 rounded-full bg-green-400 ring-2 ring-white" />
           </span>
 
-          <p v-if="!isCollapsed" class="text-gray-500 text-center pt-2" :class="{'ml-14': !isCollapsed}">John Doe</p>
+          <p v-if="!isCollapsed" class="text-gray-500 text-center pt-2" :class="{ 'ml-14': !isCollapsed }">John Doe</p>
 
         </div>
       </ul>
     </div>
 
     <!-- setting popup -->
-    <OverlayPanel ref="op"  style="height: 0px; width: 0;">
-     <div class="pop_up absolute border border-gray-100 bg-white z-5  h-44 bottom-0 mb-5 p-4 rounded-sm w-60" :class="{ ' ml-9': isCollapsed, ' ml-48': !isCollapsed }">
+    <OverlayPanel ref="op" style="height: 0px; width: 0; margin-left: 6px;" @hide="openSubMenu = false">
+
+      <div class="pop_up absolute border border-gray-100 bg-white z-5  h-44 bottom-0 mb-5 p-4 rounded-sm w-60"
+        :class="{ ' ml-8': isCollapsed, ' ml-48 ': !isCollapsed }">
 
         <ul class=" z-1">
-          <li v-for="subItem in subItems" :key="subItem.title"
-            class="rounded-lg cursor-pointer flex flex-col"
-           >
-            <div 
-              :key="subItem.title" class=" rounded-md flex px-4 py-2"
-              :class="{'bg-primaryPurple': baseRoute === subItem.route }"
-              @click="navigate(subItem.route)"
-              @mouseenter="setSubIsHovered(subItem, true)"
-              @mouseleave="setSubIsHovered(subItem, false)">
-    
+          <li v-for="subItem in subItems" :key="subItem.title" class="rounded-lg cursor-pointer flex flex-col">
+            <div :key="subItem.title" class=" rounded-md flex px-4 py-2"
+              :class="{ 'bg-primaryPurple': baseRoute === subItem.route }" @click="navigate(subItem.route)"
+              @mouseenter="setSubIsHovered(subItem, true)" @mouseleave="setSubIsHovered(subItem, false)">
+
               <i style="font-size: 1rem" class="space-y-2 "
                 :class="['pi pi-cog', 'mr-2', { 'text-primaryPurple': subItem.isHovered || baseRoute === subItem.route, 'text-gray-500': !subItem.isHovered }]">
               </i>
-           
+
               <span class=" text-base font-medium ml-2 "
                 :class="{ 'text-primaryPurple': subItem.isHovered || baseRoute === subItem.route, 'text-gray-500': !subItem.isHovered }">{{
                   subItem.title }}</span>
@@ -180,18 +131,15 @@
         </ul>
       </div>
     </OverlayPanel>
-    
+
   </div>
 </template>
   
 
 <script setup>
 import { ref } from "vue";
-import { useToast } from 'primevue/usetoast';
 import { useRouter } from 'vue-router';
 import { useMenuItems, useSubItems } from '../services/menuItems';
-
-const toast = useToast();
 
 const router = useRouter();
 
@@ -209,16 +157,13 @@ const navigate = (route) => {
   baseRoute.value = route;
 };
 
-
 // nav bar items
 const { menuItems } = useMenuItems();
 
 const { subItems } = useSubItems();
 
-
 //   mouse hover effect
 const setIsHovered = (item, val) => {
-  
   item.isHovered = val;
 };
 
@@ -229,11 +174,11 @@ const setSubIsHovered = (subItem, val) => {
 
 // check if the window is resized
 const handleResize = () => {
-  isCollapsed.value = window.innerWidth <= 768; 
+  isCollapsed.value = window.innerWidth <= 768;
 };
 
 onMounted(() => {
-  handleResize(); 
+  handleResize();
   window.addEventListener("resize", handleResize);
 });
 
@@ -250,32 +195,6 @@ const toggleCollapse = () => {
   isCollapsed.value = !isCollapsed.value;
 };
 
-const items = ref([
-  {
-      label: 'Add',
-      icon: 'pi pi-cog',
-      text:"List",
-      command: () => {
-          toast.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
-      }
-  },
-  {
-      label: 'Update',
-      icon: 'pi pi-refresh',
-      text:"User",
-      command: () => {
-          toast.add({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
-      }
-  },
-  {
-      label: 'Vue Website',
-      icon: 'pi pi-external-link',
-      text:"Archive",
-      command: () => {
-          window.location.href = 'https://vuejs.org/'
-      }
-  }
-])
 </script>
   
 <style scoped>
