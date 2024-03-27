8823226f4b16a031cf3eae737cd672fa71d5aa56
Amsalegebrehana <se.amsale.gebrehana@gmail.com>
2024-03-27 22:51:51 +0300

    (feat): tag component added

---
+++ apps/web-nuxt/src/components/shared/TagComponent.vue
@@ -0,0 +1,22 @@
+<template>
+      <div class="w-3 h-3">
+
+        <Tag  value="Business" :style="{height: '17px', fontWeight:'500',   boxShadow: '5px'}"> 
+            <template #value>
+                <span class="font-thin">{{ props.value }}</span>
+            </template>
+            </Tag>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Tag from 'primevue/tag';
+
+const props = defineProps({
+  value: {
+    type: String,
+    required: true,
+  },
+})
+</script>
