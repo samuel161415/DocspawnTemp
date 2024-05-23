9c50bd6ad7b4ece075758461ee9b8bf9145ebed0
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2024-05-23 11:48:52 +0300

    Update app.vue

--- apps/web-nuxt/src/app.vue
+++ apps/web-nuxt/src/app.vue
@@ -5,7 +5,7 @@
         
         <SideBar v-if="$route.path !== '/signin' && $route.path !== '/signup'" />
         <div class="flex flex-col w-full">
-          <TopHeader v-if="$route.path !== '/signin'" />
+          <!-- <TopHeader v-if="$route.path !== '/signin'" /> -->
           <div class="flex flex-row h-full bg-secondary p-4">
             <MenuBar v-if="isSettingsRoute($route.path)" />
             <NuxtPage />
