bdfad6aea7ee816afdd61aed2f9cf6442edcedfd
A-Hanan <a.hanan.official@gmail.com>
2024-06-23 18:46:21 +0500

    fix

--- apps/web-nuxt/src/components/settings/data_source/CreateListModal.vue
+++ apps/web-nuxt/src/components/settings/data_source/CreateListModal.vue
@@ -57,7 +57,7 @@
 <script setup>
 import { ref } from 'vue'
 
-import * as XLSX from 'xlsx'
+// import * as XLSX from 'xlsx'
 
 // import Papa from 'papaparse'
 import { useToast } from 'primevue/usetoast'
@@ -180,57 +180,57 @@ async function processFiles(data, fileType, file) {
       })
     }
     else if (['xls', 'xlsx'].includes(fileType)) {
-      try {
-      // Convert ArrayBuffer to Uint8Array
-        // const uint8Array = new Uint8Array(data)
-        const dataF = await file.arrayBuffer()
-        const uint8Array = new Uint8Array(dataF)
+      // try {
+      // // Convert ArrayBuffer to Uint8Array
+      //   // const uint8Array = new Uint8Array(data)
+      //   const dataF = await file.arrayBuffer()
+      //   const uint8Array = new Uint8Array(dataF)
 
-        // Attempt to read the workbook
-        const workbook = XLSX.read(uint8Array, { type: 'array' })
+      //   // Attempt to read the workbook
+      //   const workbook = XLSX.read(uint8Array, { type: 'array' })
 
-        // Check if the workbook contains any sheets
-        if (!workbook.SheetNames || workbook.SheetNames.length === 0)
-          throw new Error('No sheets found in the workbook.')
+      //   // Check if the workbook contains any sheets
+      //   if (!workbook.SheetNames || workbook.SheetNames.length === 0)
+      //     throw new Error('No sheets found in the workbook.')
 
-        // Get the first sheet name and the worksheet
-        const firstSheetName = workbook.SheetNames[0]
-        const worksheet = workbook.Sheets[firstSheetName]
+      //   // Get the first sheet name and the worksheet
+      //   const firstSheetName = workbook.SheetNames[0]
+      //   const worksheet = workbook.Sheets[firstSheetName]
 
-        // Convert the worksheet to JSON
-        const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
+      //   // Convert the worksheet to JSON
+      //   const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
 
-        // Check if the JSON data contains any rows
-        if (!jsonData || jsonData.length === 0)
-          throw new Error('No data found in the worksheet.')
+      //   // Check if the JSON data contains any rows
+      //   if (!jsonData || jsonData.length === 0)
+      //     throw new Error('No data found in the worksheet.')
 
-        // Assuming the first row in the Excel sheet contains headers
-        const [headers, ...dataRows] = jsonData
+      //   // Assuming the first row in the Excel sheet contains headers
+      //   const [headers, ...dataRows] = jsonData
 
-        // Check if headers are present
-        if (!headers || headers.length === 0)
-          throw new Error('No headers found in the first row of the worksheet.')
+      //   // Check if headers are present
+      //   if (!headers || headers.length === 0)
+      //     throw new Error('No headers found in the first row of the worksheet.')
 
-        // Map data rows to objects with keys based on headers
-        const formattedData = dataRows.map((row) => {
-          const rowData = {}
-          headers.forEach((header, index) => {
-            rowData[header] = row[index] !== undefined ? row[index] : ''
-          })
-          return rowData
-        })
+      //   // Map data rows to objects with keys based on headers
+      //   const formattedData = dataRows.map((row) => {
+      //     const rowData = {}
+      //     headers.forEach((header, index) => {
+      //       rowData[header] = row[index] !== undefined ? row[index] : ''
+      //     })
+      //     return rowData
+      //   })
 
-        // Add auto_index_by_docspawn to each row
-        dataSourceFileCompleteJSON.value = formattedData.map((f, i) => {
-          return { ...f, auto_index_by_docspawn: i + 1 }
-        })
-      }
-      catch (error) {
-      // Handle any errors that occurred during the process
-        console.error('An error occurred while processing the Excel file:', error.message)
-      // Optionally, you could set an error state or display a user-friendly message
-      // errorState.value = 'An error occurred while processing the Excel file.';
-      }
+      //   // Add auto_index_by_docspawn to each row
+      //   dataSourceFileCompleteJSON.value = formattedData.map((f, i) => {
+      //     return { ...f, auto_index_by_docspawn: i + 1 }
+      //   })
+      // }
+      // catch (error) {
+      // // Handle any errors that occurred during the process
+      //   console.error('An error occurred while processing the Excel file:', error.message)
+      // // Optionally, you could set an error state or display a user-friendly message
+      // // errorState.value = 'An error occurred while processing the Excel file.';
+      // }
     }
   }
 }
