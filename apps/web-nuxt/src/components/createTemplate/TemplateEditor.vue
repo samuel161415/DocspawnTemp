163af1a478d99e8234847a80c2ab6aaaf55c41c0
A-Hanan <a.hanan.official@gmail.com>
2024-06-23 18:57:13 +0500

    fix

--- apps/web-nuxt/src/components/createTemplate/TemplateEditor.vue
+++ apps/web-nuxt/src/components/createTemplate/TemplateEditor.vue
@@ -10,40 +10,40 @@
 </template>
 
 <script setup>
-import * as XLSX from 'xlsx'
+// import * as XLSX from 'xlsx'
 import TemplateFields from './templateEditor/templateFields/TemplateFields.vue'
 import Options from './templateEditor/options/Options.vue'
 import EditorCanvas from './templateEditor/editorCanvas/EditorCanvas.vue'
 import { templateEditorStore } from '@/composables/useTemplateEditorData'
 
-onMounted(() => {
-  const dataset = 'https://docspawn-bucket-1.s3.eu-central-1.amazonaws.com/docspawn-bucket-1/51b2ee2b-f2d3-4fc4-8c6e-8be78fd0a482_template_canvas_dataset.xlsx'
+// onMounted(() => {
+//   const dataset = 'https://docspawn-bucket-1.s3.eu-central-1.amazonaws.com/docspawn-bucket-1/51b2ee2b-f2d3-4fc4-8c6e-8be78fd0a482_template_canvas_dataset.xlsx'
 
-  fetch(dataset)
-    .then(response => response.arrayBuffer())
-    .then((data) => {
-      // Parse Excel file using xlsx
-      const workbook = XLSX.read(data, { type: 'array' })
-      const firstSheetName = workbook.SheetNames[0]
-      const worksheet = workbook.Sheets[firstSheetName]
-      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
+//   fetch(dataset)
+//     .then(response => response.arrayBuffer())
+//     .then((data) => {
+//       // Parse Excel file using xlsx
+//       const workbook = XLSX.read(data, { type: 'array' })
+//       const firstSheetName = workbook.SheetNames[0]
+//       const worksheet = workbook.Sheets[firstSheetName]
+//       const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
 
-      // Assuming the first row in the Excel sheet contains headers
-      const [headers, ...dataRows] = jsonData
+//       // Assuming the first row in the Excel sheet contains headers
+//       const [headers, ...dataRows] = jsonData
 
-      // Map data rows to objects with keys based on headers
-      const formattedData = dataRows.map((row) => {
-        const rowData = {}
-        headers.forEach((header, index) => {
-          rowData[header] = row[index]
-        })
+//       // Map data rows to objects with keys based on headers
+//       const formattedData = dataRows.map((row) => {
+//         const rowData = {}
+//         headers.forEach((header, index) => {
+//           rowData[header] = row[index]
+//         })
 
-        return rowData
-      })
+//         return rowData
+//       })
 
-      templateEditorStore.datasetData = { keys: Object.keys(formattedData[0]), allEntries: formattedData }
-    })
-})
+//       templateEditorStore.datasetData = { keys: Object.keys(formattedData[0]), allEntries: formattedData }
+//     })
+// })
 </script>
 
 <style lang="scss" scoped>
