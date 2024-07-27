48b2fe12163a4302e4ec2c912f18b6dc1fdd098d
A-Hanan <a.hanan.official@gmail.com>
2024-07-27 17:42:33 +0500

    fix: saving user

--- apps/web-nuxt/src/composables/useAuth.js
+++ apps/web-nuxt/src/composables/useAuth.js
@@ -1,39 +1,17 @@
 import { ref } from 'vue'
 
-const user = ref(null)
-const token = ref(null)
+const user = ref(JSON.parse(localStorage.getItem('user')) || null)
+const token = ref(localStorage.getItem('token') || null)
 
 export function useAuth() {
   const setUser = (userData) => {
     user.value = userData
+    localStorage.setItem('user', JSON.stringify(userData))
   }
 
   const setToken = (accessToken) => {
     token.value = accessToken
-  }
-
-  const fetchUserDetails = async () => {
-    if (!token.value)
-      return
-
-    try {
-      const response = await fetch('https://api.outseta.com/v1/user', {
-        headers: {
-          Authorization: `Bearer ${token.value}`,
-        //   'x-api-key': config.public.OUTSETA_API_KEY,
-        },
-      })
-
-      if (!response.ok)
-        throw new Error('Failed to fetch user details')
-
-      const data = await response.json()
-      console.log(' data at use auth >>>>>', data)
-    //   setUser(data)
-    }
-    catch (error) {
-      console.error('Error fetching user details:', error)
-    }
+    localStorage.setItem('token', accessToken)
   }
 
   return {
