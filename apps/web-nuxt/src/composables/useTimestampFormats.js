c47133c2d5381f5f8fc1abd5d33f46e5cb551213
A-Hanan <a.hanan.official@gmail.com>
2024-08-04 16:09:01 +0500

    fix: time formats

--- apps/web-nuxt/src/composables/useTimestampFormats.js
+++ apps/web-nuxt/src/composables/useTimestampFormats.js
@@ -2,7 +2,10 @@ import { ref } from 'vue'
 
 export function useTimestampFormats() {
   const timeFormats = ref([
+    { name: 'HH:MM:SS XM' },
     { name: 'HH:MM:SS' },
+    { name: 'HH:MM XM' },
+    { name: 'HH:MM' },
     { name: 'HH:SS' },
     { name: 'MM:SS' },
   ])
