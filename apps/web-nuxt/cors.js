0ca16c7ef61e0f8731e14a83566f2a50f7fbd12d
A-Hanan <a.hanan.official@gmail.com>
2024-08-21 04:25:39 +0500

    fix: vercel cors region

--- apps/web-nuxt/cors.js
+++ apps/web-nuxt/cors.js
@@ -3,7 +3,7 @@ export default function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-  //   res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Credentials', true)
 
   // Pass to next middleware
   next()
