3d962ec9379cf378b4222509a5984cbd9acc88de
A-Hanan <techaccounts@tec.tech>
2024-10-11 19:13:03 +0500

    fix: added bring front options

--- apps/web-nuxt/src/components/createTemplate/templateEditor/options/TextboxOptions.vue
+++ apps/web-nuxt/src/components/createTemplate/templateEditor/options/TextboxOptions.vue
@@ -10,7 +10,7 @@
     <p class="text-lg text-surface-500 font-normal">
       Order text boxes
     </p>
-    <p>Selected textbox will be ordered at top of layer</p>
+    <!-- <p>Selected textbox will be ordered at top of layer</p> -->
     <!-- <div v-tooltip="'Bring the selected object forward one level so it\'s hidden behind fewer objects.'" class="flex gap-2 items-center cursor-pointer " @click="bringForward">
       <font-awesome-icon icon="fa-duotone fa-solid fa-bring-forward" size="xl" style="--fa-primary-color: #009ee2; --fa-secondary-color: #009ee299; --fa-secondary-opacity: 0.6;" />
       <p>
@@ -20,12 +20,12 @@
     <div v-tooltip="'Send the selected object back one level so its hidden behind more objects.'" class="flex gap-2 items-center cursor-pointer" @click="bringBackward">
       <font-awesome-icon icon="fa-duotone fa-solid fa-send-backward" size="xl" style="--fa-primary-color: #009ee2; --fa-secondary-color: #009ee299; --fa-secondary-opacity: 0.6;" />
       <p> Send backward</p>
-    </div>
+    </div> -->
     <div v-tooltip="'Bring the selected object in front of all other objects.'" class="flex gap-2 items-center cursor-pointer" @click="bringToFront">
       <font-awesome-icon icon="fa-duotone fa-solid fa-bring-front" size="xl" style="--fa-primary-color: #009ee2; --fa-secondary-color: #009ee299; --fa-secondary-opacity: 0.6;" />
       <p> Bring to front</p>
     </div>
-    <div v-tooltip="'Send the selected object behind all other objects.'" class="flex gap-2 items-center cursor-pointer" @click="sendToBack">
+    <!-- <div v-tooltip="'Send the selected object behind all other objects.'" class="flex gap-2 items-center cursor-pointer" @click="sendToBack">
       <font-awesome-icon icon="fa-duotone fa-solid fa-send-back" size="xl" style="--fa-primary-color: #009ee2; --fa-secondary-color: #009ee299; --fa-secondary-opacity: 0.6;" />
       <p>Send to back</p>
     </div> -->
@@ -97,6 +97,7 @@ function bringToFront() {
     // Update editor container in the store
     templateEditorStore.editorContainers = templateEditorStore.editorContainers.map((c) => {
       if (c.id === activeObj.id) {
+        console.log('c id is qual to active object')
         return {
           ...c,
           style: {
