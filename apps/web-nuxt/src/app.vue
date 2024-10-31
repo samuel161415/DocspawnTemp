1809e344bf08172e93d3191d14945414abe79bca
A-Hanan <techaccounts@tec.tech>
2024-10-31 22:42:42 +0500

    fix.hanan: retriggering deployment

--- apps/web-nuxt/src/app.vue
+++ apps/web-nuxt/src/app.vue
@@ -1,4 +1,5 @@
-<!-- commit to trigger sami.listOption deployment 3 -->
+<!-- commit to trigger sami.listOption deployment 4 -->
+ <!-- commit to trigger sami.listOption deployment 5 -->
 <!-- <template>
   <div v-if="!isAuthenticated" class="flex flex-col justify-center items-center w-full h-[100vh] gap-4">
     <InputText v-model="email" placeholder="Enter your email" />
